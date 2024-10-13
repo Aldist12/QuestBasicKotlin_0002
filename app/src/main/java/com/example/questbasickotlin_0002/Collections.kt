5f49c83fc0cf5ee93048c5707b2436361817e414
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-13 23:39:56 +0700

    Menambahkan data ke dalam List Mutable

--- app/src/main/java/com/example/questbasickotlin_0002/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0002/Collections.kt
@@ -16,3 +16,7 @@ fun ContohList() {
     // List Mutable
     val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
     println(shape)
+
+    // Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
