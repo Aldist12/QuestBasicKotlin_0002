7115890b8feb9f06ae2b53f227fce4a94dbe4d00
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-14 00:26:07 +0700

    Pengertian ConditionalExpression

--- app/src/main/java/com/example/questbasickotlin_0002/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0002/ConditionalExpression.kt
@@ -1,2 +1,5 @@
 package com.example.questbasickotlin_0002
 
+// Conditional Expression
+// Conditional expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional Expression menggunakan if-else, when, dan try-catch
