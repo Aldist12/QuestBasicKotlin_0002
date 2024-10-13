b7b818062ef83ea15a58d8fefc7bafaff9203279
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-14 00:52:10 +0700

    Pengertian Data Class

--- app/src/main/java/com/example/questbasickotlin_0002/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0002/DataClass.kt
@@ -2,5 +2,5 @@ package com.example.questbasickotlin_0002
 
 // Data Class
 // Data class adalah class yang digunakan untuk menyimpan data.
-// Data class menyediakan fungsi untuk meng-overide fungsi equals(), hashCode(), dan toString().
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString().
 // Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
