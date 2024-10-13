ec4e54473a214084e41acdadcc9f8f12b125fb4a
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-13 23:38:40 +0700

    Menambahkan data pada List Mutable

--- app/src/main/java/com/example/questbasickotlin_0002/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0002/Collections.kt
@@ -12,3 +12,7 @@ fun ContohList() {
     // List Read-Only
     val readOnlyAbjad = listOf("A", "B", "C")
     println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
