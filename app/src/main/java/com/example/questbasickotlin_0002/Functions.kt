93b5d03ff8a524ff0c2f02484f7cc036f5a7603e
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-14 03:55:28 +0700

    Memanggil Fungsi withReturn

--- app/src/main/java/com/example/questbasickotlin_0002/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0002/Functions.kt
@@ -26,3 +26,7 @@ fun withDefaultParameter(name: String = "Indra", age: Int) {
     println("== withDefaultParameter")
     println("Hellp, $name! You are $age years old.")
 }
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
