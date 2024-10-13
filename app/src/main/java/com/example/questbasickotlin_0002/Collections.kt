734c1f82b7e6610cb9745ba42a690669c321747b
Aldi Septiyanto <aldi.septiyanto.ft21@mail.umy.ac.id>
2024-10-13 23:54:31 +0700

    Menambahkan data ke dalam Set Mutable

--- app/src/main/java/com/example/questbasickotlin_0002/Collections.kt
+++ app/src/main/java/com/example/questbasickotlin_0002/Collections.kt
@@ -50,3 +50,7 @@ fun ContohSet() {
     // Set Mutable
     val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
     println(shape)
+
+    // Menambahkan data ke dalam Set Mutable
+    shape.add("Rectangle")
+    println(shape)
